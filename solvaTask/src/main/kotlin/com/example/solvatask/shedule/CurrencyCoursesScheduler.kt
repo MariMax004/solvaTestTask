e1e52e23731d54f824accb23997b740cdee8c88b
mariamaksimova <Maria.ms004@mail.ru>
2023-09-24 14:54:11 +0300

    replace postConstructor

--- solvaTask/src/main/kotlin/com/example/solvatask/shedule/CurrencyCoursesScheduler.kt
+++ solvaTask/src/main/kotlin/com/example/solvatask/shedule/CurrencyCoursesScheduler.kt
@@ -1,9 +1,9 @@
 package com.example.solvatask.shedule
 
 import com.example.solvatask.service.CurrencyCourseService
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import javax.annotation.PostConstruct
 
 @Component
 class CurrencyCoursesScheduler(val currencyCourseService: CurrencyCourseService) {
@@ -11,8 +11,8 @@ class CurrencyCoursesScheduler(val currencyCourseService: CurrencyCourseService)
     val pairs = currencyCourseService.generateShortcodePairs()
 
     @Scheduled(cron = "0 0 8 * * ?")
-    @PostConstruct
-    fun initCurrencyKzt() {
+    @Bean
+    fun initCurrencyCourse() {
         for (pair in pairs) {
             val model = currencyCourseService.getCurrencyCourse(pair.first, pair.second);
             model?.let { currencyCourseService.saveCurrencyCourse(model) }
